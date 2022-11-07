61ac0e0a38340809cd9e5aff5c1dca00b5604fe7
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2022-11-07 16:18:22 -0700

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
