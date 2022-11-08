29059acfe2d022d599027dfdc89e6558d170b191
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2022-11-07 17:17:55 -0700

    routing and some eslint stuff

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAyaVu6DZ61KK7ptoKhqgRCKdP2JC-EUwY",
+  authDomain: "vue-tur.firebaseapp.com",
+  projectId: "vue-tur",
+  storageBucket: "vue-tur.appspot.com",
+  messagingSenderId: "436039500558",
+  appId: "1:436039500558:web:9ef88aa31ade6477dc0d5f",
+};
 
 firebase.initializeApp(firebaseConfig);
 
