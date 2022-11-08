7858001dc904f384392fb8531de70a62d9bffe53
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2022-11-07 20:07:04 -0700

    new chats

---
+++ src/components/ChatList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button class="button" @click="createChatRoom()">Create new chat</button>
+  </div>
+</template>
+
+<script>
+import { db } from "@/firebase";
+
+export default {
+  methods: {
+    async createChatRoom() {
+      const newChat = await db.collection("chats").add({
+        createdAt: Date.now(),
+        owner: this.uid,
+        members: [this.uid],
+      });
+      console.log(newChat);
+    },
+  },
+
+  props: ["uid"],
+};
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,17 @@
   <div>
     <h3>Home</h3>
     <User v-slot:user="{ user }">
-      <div v-if="user.value"><UserProfile :user="user" /></div>
+      <div v-if="user.value">
+        <UserProfile :user="user" />
+        <ChatList :uid="user.value.uid" />
+      </div>
       <Login v-else />
     </User>
   </div>
 </template>
 
 <script>
+import ChatList from "./ChatList.vue";
 import Login from "./Login";
 import User from "./User";
 import UserProfile from "./UserProfile";
@@ -17,6 +21,7 @@ export default {
     Login,
     User,
     UserProfile,
+    ChatList,
   },
 };
 </script>
