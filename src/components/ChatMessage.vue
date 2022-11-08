8bbe33cbc3417dd6ed61206a5ed7109ee4f22c07
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2022-11-07 21:47:40 -0700

    message styling

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message" :class="{ 'from-user': owner }">
+    {{ message.text }}
+    <br /><span class="sender">from UID {{ message.sender }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message", "owner"],
+};
+</script>
+
+<style scoped>
+.message {
+  background-color: #dddddd;
+  color: black;
+  margin-right: 10px;
+  margin-left: auto;
+  border-radius: 5px;
+  padding: 12px;
+}
+
+.from-user {
+  margin-left: 10px;
+  margin-right: auto;
+  background-color: #91bbff;
+}
+
+.sender {
+  font-size: 10px;
+}
+</style>
--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -5,11 +5,25 @@
     <User #user="{ user }">
       <ul>
         <li v-for="message of messages" :key="message.id">
-          {{ message.text }}
+          <ChatMessage
+            :message="message"
+            :owner="user.value.uid === message.sender"
+          />
         </li>
       </ul>
 
       <input class="input" v-model="newMessageText" />
+      <hr />
+      <h5>Record Audio</h5>
+
+      <button v-if="!recorder" @click="record()" class="button">
+        Record message
+      </button>
+      <button v-else @click="stop()" class="button">Stop</button>
+
+      <audio v-if="newAudio" :src="newAudioURL" controls></audio>
+
+      <hr />
 
       <button
         :disabled="!newMessageText || loading"
@@ -25,17 +39,21 @@
 
 <script>
 import User from "./User.vue";
+import ChatMessage from "./ChatMessage.vue";
 import { db } from "@/firebase";
 
 export default {
   components: {
     User,
+    ChatMessage,
   },
   data() {
     return {
       newMessageText: "",
       loading: false,
       messages: [],
+      newAudio: null,
+      recorder: null,
     };
   },
   computed: {
@@ -69,3 +87,21 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
+  background: #efefef;
+  padding: 10px;
+  border-radius: 0;
+}
+
+li {
+  display: flex;
+}
+</style>
